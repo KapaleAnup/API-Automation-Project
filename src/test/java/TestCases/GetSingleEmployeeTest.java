cb2f513f796c87e8a266fc5ca411fb52633b4699
Anup Kapale <anup.k@turtlemint.com>
2019-08-03 19:31:27 +0530

    single employee details have been added

--- src/test/java/TestCases/GetSingleEmployeeTest.java
+++ src/test/java/TestCases/GetSingleEmployeeTest.java
@@ -1,4 +1,31 @@
 package TestCases;
 
-public class GetSingleEmployeeTest {
+import CommanClass.BaseClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class GetSingleEmployeeTest  extends BaseClass {
+
+    CreateNewEmployeeTest employeeTest = new CreateNewEmployeeTest();
+
+    @Test(priority = 1, description = "Fetch only one entry by this Function.")
+    public void getEmployeeDetails(){
+
+        response = given().header("Content-Type", "application/json")
+                .when()
+                .get("/api/v1/employee/"+employeeTest.getID()+"")
+                .then().log().all().
+                        assertThat().extract().response();
+
+        if(response.statusCode() == 200 || response.statusCode() !=200){
+
+            log.info(employeeTest.getID()+ " : Employee detail is being feateched..!!");
+        }else
+        {
+            log.error("Api has an error.");
+        }
+    }
 }
